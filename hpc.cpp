e3e6882286889e4c680a7b7d04f5838fdca48653
Nicolas <nicolasthuillier@outlook.com>
2026-02-26 16:09:08 +0100

    Update hpc.cpp

--- hpc.cpp
+++ hpc.cpp
@@ -4,6 +4,8 @@
 #include "cblas.h"
 using namespace std;
 
+// Cblas (replace "d" by "z" if we deal with complex)
+
 // y <-- ax+y
 cblas_daxpy(n,a,x,inc x,y,inc y);
 
@@ -18,5 +20,13 @@ cblas_dnrm2(n,x,inc x);
 
 // x <-- ax
 cblas_dscal(n,a,x,inc x);
+
+// y <-- aAx + by
+// Order = CblasRowMajor or CblasColMajor, TransA = CblasNoTrans or CblasTrans, lda = nb colonnes (en row major) 
+cblas_dgem(Order,TransA,M,N,a,A,lda,x,inc x,b,y,inc y);
+
+// C <-- aAB+bC
+cblas_dgemm(Order,TransA,TransB,M,N,K,a,A,lda,B,ldb,b,C,ldc);
+
 
 
